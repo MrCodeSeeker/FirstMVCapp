90b2867a227e6b49860444ab7e22a9cd0621e0da
DESKTOP-U53QOS1\Poodh <praveen.poodhari@gmail.com>
2024-03-22 15:00:39 +0530

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        [Route("api/HelloWorld/Index")]
+        public IActionResult Index()
+        {
+            DogViewModel dg = new DogViewModel() { Name = "Bishop", Age = 11 };
+            return View(dg);
+        }
+        public string dog()
+        { return "Who's there"; }
+        public IActionResult create()
+        {
+            return View();  
+        }
+    }
+}
